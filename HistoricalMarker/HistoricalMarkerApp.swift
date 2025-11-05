22bc581e2f1f015f1b36438bce0c0d4d7ea37b10
Christopher Gillam <cgillam0513@gmail.com>
2025-11-05 10:25:05 -0600

    Initial Commit

---
+++ HistoricalMarker/HistoricalMarkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistoricalMarkerApp.swift
+//  HistoricalMarker
+//
+//  Created by Christopher Gillam on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct HistoricalMarkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
